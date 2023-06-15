7214299c18e5e399145c34946d588d84df00bb8c
darkluminance <raiyan.abrar1008@gmail.com>
2023-06-15 11:41:07 +0600

    Commit 03
    1. Changed text in about page
    2. photo gallery updated with blur loader

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -13,15 +13,13 @@ export default function About() {
 					<div className="aboutTextContent">
 						<h1 className="pageHeader">About Me</h1>
 						<p id="p1">
-							I’m Raiyan Abrar. A Programmer, Problem solver, Creative Person,
-							Designer, and a Cat Lover.
+							I’m Raiyan Abrar. A Programmer, a Designer, a Problem solver, a
+							Creative Person, and a Cat Lover.
 						</p>
 						<p id="p2">
 							I have completed my B.Sc. in Computer Science and Engineering in
-							2023 from Dhaka.
-							<br></br>
-							I'm currently working as a Software Developer at RedDot Digital
-							Limited.
+							2023 from Dhaka and am currently working as a Software Developer
+							at RedDot Digital Limited.
 						</p>
 						<p id="p3">
 							Born on August 23rd in Dhaka, Bangladesh, I have a passion for
@@ -32,8 +30,8 @@ export default function About() {
 						</p>
 						<p id="p4">
 							With a strong background in programming and design, I have both
-							personal and academic projects under my belt ranging from web and
-							mobile development to game design. I am particularly proud of my
+							personal and academic projects under my belt ranging from software
+							development to game design. I am particularly proud of my
 							achievement in releasing one of the first indie fan-made Pokémon
 							games in Bangladesh at the age of 19.
 						</p>
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,31 +7,87 @@ import close from '@/assets/images/close.png';
 
 export default function Gallery() {
 	const [photos, setPhotos] = useState([]);
+	const [smallphotos, setsmallPhotos] = useState([]);
+	const originalURL =
+		'https://res.cloudinary.com/dwyosqxlr/image/list/gallery-xl.json';
+	const smallURL =
+		'https://res.cloudinary.com/dwyosqxlr/image/list/gallery-sm.json';
 
 	const fetchData = () => {
-		return axios
-			.get('https://res.cloudinary.com/dwyosqxlr/image/list/gallery.json')
-			.then((response) => {
-				let data = response.data.resources;
-				// data = data.sort(function (a, b) {
-				// 	return a.created_at - b.created_at;
+		return axios.get(originalURL).then((response) => {
+			let data = response.data.resources;
+			// data = data.sort(function (a, b) {
+			// 	return a.created_at - b.created_at;
+			// });
+			let url = {};
+
+			data.forEach((element) => {
+				const key = element.public_id.slice(11);
+				// url.push({
+				// 	key:
+				// 		'https://res.cloudinary.com/dwyosqxlr/image/upload/v' +
+				// 		element.version +
+				// 		'/' +
+				// 		element.public_id +
+				// 		'.' +
+				// 		element.format,
 				// });
-				let url = [];
-
-				data.forEach((element) => {
-					url.push(
-						'https://res.cloudinary.com/dwyosqxlr/image/upload/v1680369628/' +
-							element.public_id +
-							'.' +
-							element.format
-					);
-				});
-
-				setPhotos(url);
+				url[key] =
+					'https://res.cloudinary.com/dwyosqxlr/image/upload/v' +
+					element.version +
+					'/' +
+					element.public_id +
+					'.' +
+					element.format;
 			});
+
+			console.log(url);
+			setPhotos(url);
+		});
+	};
+
+	const fetchsmallData = () => {
+		return axios.get(smallURL).then((response) => {
+			let data = response.data.resources;
+			// data = data.sort(function (a, b) {
+			// 	return a.created_at - b.created_at;
+			// });
+			let url = [];
+
+			data.forEach((element) => {
+				// url.push(
+				// 	'https://res.cloudinary.com/dwyosqxlr/image/upload/v' +
+				// 		element.version +
+				// 		'/' +
+				// 		element.public_id +
+				// 		'.' +
+				// 		element.format
+				// );
+				const key = element.public_id.slice(11).replace('_small', '');
+				// url.push({
+				// 	key:
+				// 		'https://res.cloudinary.com/dwyosqxlr/image/upload/v' +
+				// 		element.version +
+				// 		'/' +
+				// 		element.public_id +
+				// 		'.' +
+				// 		element.format,
+				// });
+				url[key] =
+					'https://res.cloudinary.com/dwyosqxlr/image/upload/v' +
+					element.version +
+					'/' +
+					element.public_id +
+					'.' +
+					element.format;
+			});
+			console.log(url);
+			setsmallPhotos(url);
+		});
 	};
 
 	useEffect(() => {
+		fetchsmallData();
 		fetchData();
 	}, []);
 
@@ -45,9 +101,9 @@ export default function Gallery() {
 
 	return (
 		<div className="galleryContainer ">
-			{/* <div className={model ? 'model imgViewer' : 'model'}>
-				<img src={tmpImgSrc} alt="" />
-				<LazyLoadImage width={800} src={tmpImgSrc} />
+			<div className={model ? 'model imgViewer flex-center-full' : 'model'}>
+				{/* <img src={tmpImgSrc} alt="" /> */}
+				<img src={tmpImgSrc} />
 				<Image
 					onClick={() => {
 						setModel(false);
@@ -55,14 +111,19 @@ export default function Gallery() {
 					src={close}
 					alt=""
 				/>
-			</div> */}
+			</div>
 			<div className="gallery">
-				{photos.map((image, index) => {
+				{Object.keys(photos).map((key, index) => {
 					return (
-						<div className="pics" key={index} onClick={() => getImg(image)}>
+						<div
+							className="pics"
+							style={{ backgroundImage: 'url(' + smallphotos[key] + ')' }}
+							key={key}
+							onClick={() => getImg(photos[key])}
+						>
 							{/* {image} */}
 							{/* <Image src={image} alt="" width={800} /> */}
-							<img src={image} alt="" />
+							<img src={photos[key]} alt="" loading="lazy" />
 						</div>
 					);
 				})}
